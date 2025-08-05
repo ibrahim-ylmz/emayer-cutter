73d38794d08497e7a213c28d2b125520501b4573
ibrahim-ylmz <ibrahimyilmaz1498@gmail.com>
2025-08-05 18:28:59 +0300

    Add window manager initialization and configuration to application startup

--- lib/core/init/application_initialize.dart
+++ lib/core/init/application_initialize.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 @immutable
 /// This class is used initialize the application process
@@ -7,11 +8,21 @@ final class ApplicationInitialize {
   /// Project basic required initialize
   Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
     await runZonedGuarded<Future<void>>(_initialize, (error, stack) {});
   }
 
   /// This method is used to initialize the application process
   Future<void> _initialize() async {
-    
+    WindowOptions windowOptions = const WindowOptions(
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.hidden,
+      fullScreen: true,
+      windowButtonVisibility: false,
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
   }
 }
