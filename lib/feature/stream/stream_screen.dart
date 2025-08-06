c7b44c1aec83da5de915c31923fd9d23513e7b5f
ibrahim-ylmz <ibrahimyilmaz1498@gmail.com>
2025-08-06 15:48:06 +0300

    Stream2

--- lib/feature/stream/stream_screen.dart
+++ lib/feature/stream/stream_screen.dart
@@ -21,15 +21,16 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
     final streamNotifier = Provider.of<StreamNotifier>(context, listen: false);
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: 14.w,
-          right: 14.w,
-          top: 105.h,
-          bottom: 105.h,
-        ),
-        child: Stack(
-          children: [
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          height: 1400.h, // Limited height for portrait
+          margin: EdgeInsets.symmetric(
+            horizontal: 20.w,
+            vertical: 60.h,
+          ),
+          child: Stack(
+            children: [
             ClipRRect(
                 borderRadius: BorderRadius.circular(16.r),
                 child: GestureDetector(
@@ -52,34 +53,38 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
                         streamNotifier.changeIsPlaying(false);
                       }
                     },
-                    child: Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                              colors: [
-                                Colors.grey,
-                                Colors.black,
-                              ],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter),
-                        ),
-                        child: streamNotifierListen.isSocketConnected
-                            ? imageData.isNotEmpty
-                                ? RepaintBoundary(
-                                    child: Image.memory(
-                                      gaplessPlayback: true,
-                                      alignment: Alignment.center,
-                                      filterQuality: FilterQuality.high,
-                                      width: double.infinity,
-                                      imageData,
-                                      fit: BoxFit.contain,
-                                    ),
-                                  )
-                                : const Center(
-                                    child: CircularProgressIndicator(),
-                                  )
-                            : const Center(
-                                child: CircularProgressIndicator(),
-                              )))),
+                    child: Center(
+                      child: Container(
+                          width: double.infinity,
+                          height: 800.h, // Limited height for video content
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [
+                                  Colors.grey,
+                                  Colors.black,
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter),
+                          ),
+                          child: streamNotifierListen.isSocketConnected
+                              ? imageData.isNotEmpty
+                                  ? RepaintBoundary(
+                                      child: Image.memory(
+                                        gaplessPlayback: true,
+                                        alignment: Alignment.center,
+                                        filterQuality: FilterQuality.high,
+                                        width: double.infinity,
+                                        imageData,
+                                        fit: BoxFit.contain,
+                                      ),
+                                    )
+                                  : const Center(
+                                      child: CircularProgressIndicator(),
+                                    )
+                              : const Center(
+                                  child: CircularProgressIndicator(),
+                                )),
+                    ))),
             Positioned(
                 bottom: 38.h,
                 left: 59.w,
@@ -90,19 +95,19 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
                     Text(
                       'Top Cam',
                       style: TextStyle(
-                        fontSize: 15.s,
+                        fontSize: 18.sp,
                         color: Colors.white,
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.w600,
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(top: 10.h),
+                      padding: EdgeInsets.only(top: 8.h),
                       child: Container(
-                        width: 45.w,
-                        height: 58.h,
+                        width: 90.w,
+                        height: 32.h,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(13.r),
+                          borderRadius: BorderRadius.circular(16.r),
                           color: streamNotifierListen.isSocketConnected
                               ? const Color.fromRGBO(33, 184, 42, 1)
                               : Colors.red[700],
@@ -112,10 +117,10 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             CircleAvatar(
-                              radius: 2.r,
+                              radius: 4.r,
                               backgroundColor: Colors.white,
                             ),
-                            SizedBox(width: 3.w),
+                            SizedBox(width: 6.w),
                             Text(
                               streamNotifierListen.isSocketConnected
                                   ? "Online"
@@ -123,7 +128,7 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
                               style: TextStyle(
                                 fontFamily: "Roboto",
                                 color: Colors.white,
-                                fontSize: 8.sp,
+                                fontSize: 12.sp,
                               ),
                             )
                           ],
@@ -159,20 +164,20 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
                   ];
                 },
                 child: Container(
-                  height: 106.h,
-                  width: 38.w,
+                  height: 48.h,
+                  width: 48.w,
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(
                       color: const Color.fromRGBO(255, 255, 255, 0.47),
-                      width: 2,
+                      width: 1.5.w,
                     ),
                   ),
                   child: Center(
                     child: Icon(
                       Icons.more_vert_rounded,
                       color: Colors.white,
-                      size: 17.s,
+                      size: 20.s,
                     ),
                   ),
                 ),
@@ -203,8 +208,8 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
                 opacity: streamNotifierListen.isPlaying ? 0 : 1,
                 duration: Durations.short3,
                 child: Container(
-                    height: 250.h,
-                    width: 140.w,
+                    height: 120.h,
+                    width: 120.w,
                     decoration: const BoxDecoration(
                         shape: BoxShape.circle,
                         color: Color.fromRGBO(48, 57, 63, 1)),
@@ -213,16 +218,17 @@ class _StreamScreenState extends State<StreamScreen> with StreamScreenMixin {
                             ? Icon(
                                 Icons.pause,
                                 color: Colors.white,
-                                size: 59.s,
+                                size: 48.s,
                               )
                             : Icon(
                                 Icons.play_arrow_rounded,
                                 color: Colors.white,
-                                size: 59.s,
+                                size: 48.s,
                               ))),
               )),
             ),
-          ],
+            ],
+          ),
         ),
       ),
     );
